432019ea77a828b941ef01814023f1c986b37cab
Shining <shiining@linuxcondom.net>
2019-02-07 19:04:58 +0100

    Forced (re)installation added

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 rm -fr build
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make
 cd ..
-kpackagetool5 -t Plasma/Applet --remove net.linuxcondom.plasma.koolmorning
-kpackagetool5 -t Plasma/Applet --install package
+if [ "x$1" = "x-f" ] ; then
+	kpackagetool5 -t Plasma/Applet --remove net.linuxcondom.plasma.koolmorning
+	kpackagetool5 -t Plasma/Applet --upgrade package && echo "Package reinstalled"
+elif [ -d "$HOME/.local/share/plasma/plasmoids/net.linuxcondom.plasma.koolmorning" ] ; then
+	kpackagetool5 -t Plasma/Applet --upgrade package && echo "Package upgraded"
+else
+	kpackagetool5 -t Plasma/Applet --install package && echo "Package installed"
+fi
