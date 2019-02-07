0ec5a7939eb82389e8c0f432630a89d4efcc9e00
Shining <shiining@linuxcondom.net>
2019-02-07 20:02:19 +0100

    Fixed the forced (re)installation

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ make
 cd ..
 if [ "x$1" = "x-f" ] ; then
 	kpackagetool5 -t Plasma/Applet --remove net.linuxcondom.plasma.koolmorning
-	kpackagetool5 -t Plasma/Applet --upgrade package && echo "Package reinstalled"
+	kpackagetool5 -t Plasma/Applet --install package && echo "Package reinstalled"
 elif [ -d "$HOME/.local/share/plasma/plasmoids/net.linuxcondom.plasma.koolmorning" ] ; then
 	kpackagetool5 -t Plasma/Applet --upgrade package && echo "Package upgraded"
 else
